23609d031a1da2fb61d15a83e1e9e06e82af604b
Supriya Kumari <supriya17feb99@gmail.com>
2019-09-07 11:03:18 +0530

    Initial commit

---
+++ app/src/main/java/com/supriya/workmanager/NotificationWorker.kt
@@ -0,0 +1,28 @@
+package com.supriya.workmanager
+
+import android.app.Notification
+import android.app.NotificationManager
+import android.content.Context
+import androidx.core.app.NotificationCompat
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class NotificationWorker(val context: Context, workParams: WorkerParameters) : Worker(context, workParams) {
+    override fun doWork(): Result {
+
+        val nm = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val notification = NotificationCompat.Builder(context,"first").apply {
+            setContentTitle("Background Task")
+            setContentText("Simple notification")
+            setSmallIcon(R.drawable.ic_launcher_foreground)
+            priority = NotificationManager.IMPORTANCE_HIGH
+            setDefaults(Notification.DEFAULT_ALL)
+        }.build()
+
+        nm.notify(System.currentTimeMillis().toInt(),notification)
+
+        return Result.success()
+    }
+
+}
